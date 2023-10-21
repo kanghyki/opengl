21d5607ae1ada8485faf092edd7607b27e075585
kanghyki <kanghyki@gmail.com>
2023-10-22 02:02:07 +0900

    chore: header

--- inc/main.hpp
+++ inc/main.hpp
@@ -1,22 +1,7 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+#include "./opengl.hpp"
 #include <iostream>
-
-
-/*
- * Unabled deprecated warning
- */
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-/*
- * OpenGL
- */
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#include <OpenGL/glext.h>
-/*
- * GLUT
- */
-#include <GLUT/glut.h>
 
 #endif
---
+++ inc/opengl.hpp
@@ -0,0 +1,23 @@
+#ifndef OPENGL_HPP
+#define OPENGL_HPP
+
+/*
+ * Unabled deprecated warning
+    #pragma GCC diagnostic push
+    ...
+    #pragma GCC diagnostic pop
+ */
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+/*
+ * OpenGL
+ */
+#include <OpenGL/gl.h>
+// #include <OpenGL/glu.h>
+// #include <OpenGL/glext.h>
+
+/*
+ * GLUT
+ */
+#include <GLUT/glut.h>
+
+#endif
